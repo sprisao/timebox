d5a917e77a6887a1b5ac619393f02fb356b08031
brucedev <whdtmek@gmail.com>
2023-12-10 09:33:46 +0900

    Install ReactIcon

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import {Button} from "@/components/ui/button";
 import BR_InputBox from "@/components/ui/inputBox_template";
+import { HiHeart, HiOutlineBookmark } from "react-icons/hi2";
 
 export default function Home() {
     return (
@@ -7,10 +8,17 @@ export default function Home() {
             <h1>
                 Hello Bruce
             </h1>
+            <h2>Classname Customizing Utils</h2>
             <BR_InputBox placeHolder="hi"/>
+            <h2>Shadcn_Button</h2>
             <Button>
                 <a>Shadcn</a>
             </Button>
+            <h2>React Icons</h2>
+            <HiHeart />
+            <HiOutlineBookmark />
+
         </>
+
     )
 }
